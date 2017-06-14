0c4e5199dee265ffa178409c0062740ba5e41151
zeno <amarzeno@outlook.com>
2017-06-13 23:19:28 -0400

    line trace added

--- TankWar/Source/TankWar/TankPlayerController.cpp
+++ TankWar/Source/TankWar/TankPlayerController.cpp
@@ -48,6 +48,50 @@ void ATankPlayerController::AimTowardsCrossHair()
 // Get world location if linetrace through crosshair, true if hits landscape
 bool ATankPlayerController::GetSightRayHitLocation(FVector& HitLocation)
 {
-	HitLocation = FVector(1.0);
+	// Find the crosshair position
+	int32 ViewportSizeX, ViewportSizeY;
+	GetViewportSize(ViewportSizeX, ViewportSizeY);
+	auto ScreenLocation = FVector2D(ViewportSizeX * CrossHairXLocation, ViewportSizeY * CrossHairYLocation);
+
+	FVector LookDirection;
+	if (GetLookDirection(ScreenLocation, LookDirection)) 
+	{
+		// Line trace along that lookdirection, and see what we hit
+		GetLookVectorHitLocation(LookDirection, HitLocation);
+	}
+	
+	//UE_LOG(LogTemp, Warning, TEXT("ScreenLocation is : %s"), *ScreenLocation.ToString())
+	// "De-project" the screen position of the cross hair to a world direction
+	// Line-trace along that look direction, and see what we hit (upto max range)
 	return true;
+}
+
+bool ATankPlayerController::GetLookDirection(FVector2D ScreenLocation, FVector& LookDirection) const
+{
+	FVector CameraWorldLocation; // To be discarded
+	return DeprojectScreenPositionToWorld(
+		ScreenLocation.X,
+		ScreenLocation.Y,
+		CameraWorldLocation,
+		LookDirection
+	);
+}
+
+bool ATankPlayerController::GetLookVectorHitLocation(FVector LookDirection, FVector& HitLocation) const
+{
+	FHitResult HitResult;
+	auto StartLocation = PlayerCameraManager->GetCameraLocation();
+	auto EndLocation = StartLocation + (LookDirection * LineTraceRange);
+	if(GetWorld()->LineTraceSingleByChannel(
+		HitResult,
+		StartLocation,
+		EndLocation,
+		ECollisionChannel::ECC_Visibility)
+		)
+	{
+		HitLocation = HitResult.Location;
+		return true;
+	}
+	HitLocation = FVector(0);
+	return false; // line trace didnt succeed
 }
--- TankWar/Source/TankWar/TankPlayerController.h
+++ TankWar/Source/TankWar/TankPlayerController.h
@@ -14,15 +14,22 @@ class TANKWAR_API ATankPlayerController : public APlayerController
 {
 	GENERATED_BODY()
 	
-public:
+private:
 	ATank* GetControlledTank() const;
 	
 	virtual void BeginPlay() override;
 	virtual void Tick(float DeltaTime) override;
 
-private:
+	UPROPERTY(EditAnywhere)
+	float CrossHairXLocation = 0.5f;
+	UPROPERTY(EditAnywhere)
+	float CrossHairYLocation = 0.3333f;
 	// Start the tank moving the barrel so that a shot
 	// would hit where the world is
 	void AimTowardsCrossHair();
 	bool GetSightRayHitLocation(FVector& HitLocation);
+	bool GetLookDirection(FVector2D ScreenLocation, FVector& LookDirection) const;
+	bool GetLookVectorHitLocation(FVector LookDirection, FVector& HitLocation) const;
+	UPROPERTY(EditAnywhere)
+	float LineTraceRange = 1000000;
 };
